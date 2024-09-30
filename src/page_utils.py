2ca57470ee2bc924a5fb52eb036bf063a25ea76c
Aleksey_Milyaev <Trane94.milaev@yandex.ru>
2024-09-30 23:21:07 +0700

    finish views

--- src/page_utils.py
+++ src/page_utils.py
@@ -1,13 +1,17 @@
 import json
 import os
+import time
 
 from collections import Counter
 from datetime import datetime
-
+import requests
+from dotenv import load_dotenv
 import pandas as pd
 
 PATH_TRANSACTIONS = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.xlsx")
 PATH_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "main_page.json")
+load_dotenv()
+
 
 def get_gritting(user_date: str):
     """Функция возвращающая приветствие"""
@@ -32,7 +36,8 @@ def read_transactions(user_date: str) -> list:
     transactions_from_date = []
     for i in range(date.day):
         day = f"{date.year}-{date.month}-{i+1} 00:00:00"
-        exp = excel_transactions[excel_transactions["Дата платежа"] == datetime.strftime(datetime.strptime(day, "%Y-%m-%d %H:%M:%S"), "%d.%m.%Y")]
+        exp = excel_transactions[excel_transactions["Дата платежа"] ==
+                                 datetime.strftime(datetime.strptime(day, "%Y-%m-%d %H:%M:%S"), "%d.%m.%Y")]
         transactions_from_date.append(exp.to_dict(orient='records'))
 
     transaction = []
@@ -75,8 +80,35 @@ def card_info(transactions: list, card_number: list) -> dict:
 
 def top_transaction(transactions: list) -> list:
     """Функция возвращающая пять наибольших операций"""
-    top_five = []
-    for transaction in transactions:
+
+    transactions = sorted(transactions, key=lambda x: abs(x['Сумма операции']), reverse=True)
+    top_five = transactions[:5]
+
+    return top_five
 
 
-        return []
+def currency_rate(rate: list) -> list:
+    """Функция вывода курса валют с сайта 'Abstract API'"""
+
+    currency = []
+    abstract_api_key = os.getenv("abstract_api")
+    for item in rate:
+        content = requests.get(f"https://exchange-rates.abstractapi.com/v1/live/?api_key={abstract_api_key}&base={item}&target=RUB").text
+        response = json.loads(content)
+        currency.append({"currency": item, "rate": round(response["exchange_rates"]["RUB"], 2)})
+        time.sleep(1)
+
+    return currency
+
+
+def stock_prices(stock: list) -> list:
+    """Функция вывода стоимости акций с сайта 'marketstack'"""
+
+    stocks = []
+    marketstack_api_key = os.getenv("marketstack_api")
+    for item in stock:
+        content = requests.get(f"http://api.marketstack.com/v1/eod?access_key={marketstack_api_key}&symbols={item}").text
+        response = json.loads(content)
+        stocks.append({"stock": item, "price": response["data"][0]['open']})
+
+    return [stocks]
--- src/views.py
+++ src/views.py
@@ -1,21 +1,41 @@
 import json
 import os
-from page_utils import card_info, read_transactions, count_card, get_gritting
+from page_utils import (card_info, read_transactions, count_card, get_gritting, top_transaction, currency_rate,
+                        stock_prices)
 
-PATH_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "main_page.json")
+PATH_MAIN_PAGE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "main_page.json")
+PATH_USERS_SETTINGS = os.path.join(os.path.dirname(os.path.dirname(__file__)), "users_settings.json")
 
 
 def main_page(user_date: str):
     """Функция возвращающая json файл с информацией по картам, транзакциям, курсе валют, акциях"""
 
-    transactions = read_transactions("2018-12-30 22:13:13")
+    with open(PATH_USERS_SETTINGS, encoding="UTF8") as file:
+        content = file.read()
+        rate_stock = json.loads(content)
+
+    rats = rate_stock["user_currencies"]
+    stock = rate_stock["user_stocks"]
+    transactions = read_transactions(user_date)
     card_count = count_card(transactions)
     information_card = []
+    top_five = top_transaction(transactions)
+    top_transactions = []
+    currency = currency_rate(rats)
+    stocks = stock_prices(stock)
     for card in card_count:
         information_card.append(card_info(transactions, card))
-    response = [{"gritting": get_gritting(user_date)}, {"cards": information_card}, {"top_transactions": []}]
-    with open(PATH_FILE, "w", encoding="UTF8") as file:
+
+    for item in top_five:
+        operation = {"date": item['Дата платежа'], "amount": item['Сумма операции'],
+                     "category": item['Категория'], "description": item['Описание']}
+        top_transactions.append(operation)
+
+    response = [{"gritting": get_gritting(user_date), "cards": information_card},
+                {"top_transactions": top_transactions}, {"currency_rates": currency}, {"stock_prices": stocks}]
+
+    with open(PATH_MAIN_PAGE, "w", encoding="UTF8") as file:
         json.dump(response, file, indent=4, ensure_ascii=False)
 
 
-main_page("2020-12-12 22:13:13")
+main_page("2019-12-12 22:13:13")
